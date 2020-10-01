5deea13cfa0952de27f202650eb99cb98c1061de
Raymond Li <Rlii0303@gmail.com>
2020-10-01 16:31:51 -0700

    changed

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -11,30 +11,30 @@ module.exports = app => {
   app.get(
     `/auth/google/callback`,
     passport.authenticate(`google`),
-    async (req, res, next) => {
+    async (req, res) => {
       try {
         backURL = req.header("Referer") || "/";
         res.redirect(backURL);
       } catch (error) {
-        next(error);
+        throw error;
       }
     }
   );
 
-  app.get(`/api/logout`, async (req, res, next) => {
+  app.get(`/api/logout`, async (req, res) => {
     try {
       req.logout();
       res.redirect(`back`);
     } catch (error) {
-      next(error);
+      throw error;
     }
   });
 
-  app.get(`/api/currentUser`, async (req, res, next) => {
+  app.get(`/api/currentUser`, async (req, res) => {
     try {
       res.send(req.user);
     } catch (error) {
-      next(error);
+      throw error;
     }
   });
 };
